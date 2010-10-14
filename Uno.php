5c23c9a59219875fc2b2e96d8d44303102f0527f
François Poirotte <clicky@erebot.net>
2010-10-14 17:45:15 +0000

    Replace modules/*/ includes with calls to __DIR__.

--- Uno.php
+++ Uno.php
@@ -1,6 +1,16 @@
 <?php
 
-include_once('modules/Uno/src/game.php');
+if (!defined('__DIR__')) {
+  class __FILE_CLASS__ {
+    function  __toString() {
+      $X = debug_backtrace();
+      return dirname($X[1]['file']);
+    }
+  }
+  define('__DIR__', new __FILE_CLASS__);
+} 
+
+include_once(__DIR__.'/src/game.php');
 include_once('src/styling.php');
 
 class   ErebotModule_Uno
