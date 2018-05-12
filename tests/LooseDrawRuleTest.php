918710a91cce790e8431805538780466d5bf2e48
François Poirotte <clicky@erebot.net>
2018-05-12 19:32:50 +0200

    Remove unused test files

--- tests/LooseDrawRuleTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once(dirname(__FILE__).'/utils.php');
-
-#class UnoLooseDrawRuleTest
-#extends \PHPUnit\Framework\TestCase
-#{
-#}
-
--- tests/UnlimitedDeckRuleTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once(dirname(__FILE__).'/utils.php');
-
-#class UnoUnlimitedDeckRuleTest
-#extends \PHPUnit\Framework\TestCase
-#{
-#}
-
